06720e385a1ee726bb1ca23fd550ce57a831b886
brudermusscode <justinleonseidel@proton.me>
2022-05-10 21:26:07 +0200

    Add definition

--- config/definitions.php
+++ config/definitions.php
@@ -4,4 +4,4 @@ define("PREROOT", dirname($_SERVER["DOCUMENT_ROOT"]));
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
 define("TEMPLATES", $_SERVER["DOCUMENT_ROOT"] . '/app/templates');
 define("JSON_RESPONSE_FORMAT", 'Content-type: application/json');
-define("404", 'location: /404');
+define("NOT_FOUND", 'location: /404');
